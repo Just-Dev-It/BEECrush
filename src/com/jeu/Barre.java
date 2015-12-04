8eac83c52d1354ed18719ac18949788401878f72
sowoumar25 <sowoumar25@yahoo.fr>
2015-12-04 04:18:47 +0100

    implementation Classe Barre

--- src/com/jeu/Barre.java
+++ src/com/jeu/Barre.java
@@ -1,5 +1,32 @@
 package com.jeu;
 
 public class Barre {
+	protected int max;
+	protected int miel;
+	protected int progression;
+	
+	public Barre(int max){
+		this.max = max;
+	}
 
+	public void init(){
+		this.miel = 0;
+	}
+	
+	public void setMiel(int m){
+		this.miel = m;
+		initImage();
+	}
+	
+	public int getMiel(){
+		return this.miel;
+	}
+	
+	public int getMielMax(){
+		return this.max;
+	}
+	
+	public void initImage(){
+		progression = (max-miel)/max;
+	}
 }
